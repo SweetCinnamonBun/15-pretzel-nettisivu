fbd2872ce516ee7eba42aa0fc368556177e70f1f
Roman <roman.klemiato18@gmail.com>
2025-03-22 21:17:32 +0200

    deployment stuff

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/15-pretzel-nettisivu",
 
   server: {
     port: 3000,
